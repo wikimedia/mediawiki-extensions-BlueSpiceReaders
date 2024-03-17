30a9fca37be19aa2cd4f9cede307d7780a6643de
libraryupgrader <tools.libraryupgrader@tools.wmflabs.org>
2024-03-17 14:42:29 +0000

    build: Updating mediawiki/mediawiki-codesniffer to 43.0.0
    Change-Id: If13037018c2264a3e04bed1715b68b5751ca41e0

--- src/ConfigDefinition/ReadersNumOfReaders.php
+++ src/ConfigDefinition/ReadersNumOfReaders.php
@@ -13,7 +13,7 @@ class ReadersNumOfReaders extends IntSetting {
 	public function getPaths() {
 		return [
 			static::MAIN_PATH_FEATURE . '/' . static::FEATURE_PERSONALISATION . '/BlueSpiceReaders',
-			static::MAIN_PATH_EXTENSION . '/BlueSpiceReaders/' . static::FEATURE_PERSONALISATION ,
+			static::MAIN_PATH_EXTENSION . '/BlueSpiceReaders/' . static::FEATURE_PERSONALISATION,
 			static::MAIN_PATH_PACKAGE . '/' . static::PACKAGE_FREE . '/BlueSpiceReaders',
 		];
 	}
