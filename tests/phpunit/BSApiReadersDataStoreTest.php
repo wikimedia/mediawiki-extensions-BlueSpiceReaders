c0509cc679167d4771b92251587b186e5c5bcfb6
hslater <hamish.slater@hallowelt.com>
2025-05-07 15:44:40 +1200

    Fix BSApiReadersDataStoreTest
    ERM20742
    
    [5.1.x]
    
    Change-Id: Ib8bef9c0c166e2fddd1b8a56a8000459d6bcd3ac

--- tests/phpunit/BSApiReadersDataStoreTest.php
+++ tests/phpunit/BSApiReadersDataStoreTest.php
@@ -1,5 +1,7 @@
 <?php
 
+use BlueSpice\Tests\BSApiExtJSStoreTestBase;
+
 /**
  * @group medium
  * @group API
@@ -9,13 +11,13 @@
  * @group BlueSpiceReaders
  * @covers BSApiReadersDataStore
  */
-class BSApiReadersDataStoreTest extends \BlueSpice\Tests\BSApiExtJSStoreTestBase {
-	/** @var int */
-	protected $iFixtureTotal = 4;
-	// Thats the ID of UTSysop
+class BSApiReadersDataStoreTest extends BSApiExtJSStoreTestBase {
 
 	/** @var int */
-	protected $sQuery = 1;
+	protected $iFixtureTotal = 1;
+
+	/** @var string */
+	protected $sQuery = '1';
 
 	protected function getStoreSchema() {
 		return [
@@ -40,33 +42,33 @@ class BSApiReadersDataStoreTest extends \BlueSpice\Tests\BSApiExtJSStoreTestBase
 		];
 	}
 
-	protected function setUp(): void {
-		parent::setUp();
-
-		$oPageFixtures = new \BlueSpice\Tests\BSPageFixturesProvider();
-		$aFixtures = $oPageFixtures->getFixtureData();
-		foreach ( $aFixtures as $aFixture ) {
-			$this->insertPage( $aFixture[0], $aFixture[1] );
-		}
-
-		new BSReadersFixtures( $this->db );
-	}
-
 	protected function createStoreFixtureData() {
+		$dbw = $this->getDb();
+		$this->setUp();
+
+		$pageID = $this->insertPage( 'Test' )['id'];
+		$dbw->insert(
+			'bs_readers',
+			[
+				'readers_id' => 1,
+				'readers_user_id' => 1,
+				'readers_user_name' => 'WikiSysop',
+				'readers_page_id' => $pageID,
+				'readers_rev_id' => 1,
+				'readers_ts' => '20250101010101'
+			],
+			__METHOD__
+		);
 	}
 
 	protected function getModuleName() {
 		return 'bs-readers-data-store';
 	}
 
-	public function providePagingData() {
-		return parent::providePagingData();
-	}
-
 	public function provideSingleFilterData() {
 		return [
 			'Filter by pv_page_title equals' => [ 'string', 'eq', 'pv_page_title', 'Test', 1 ],
-			'Filter by pv_page_title contains' => [ 'string', 'ct', 'pv_page_title', 'est', 2 ]
+			'Filter by pv_page_title contains' => [ 'string', 'ct', 'pv_page_title', 'est', 1 ]
 		];
 	}
 
@@ -78,8 +80,7 @@ class BSApiReadersDataStoreTest extends \BlueSpice\Tests\BSApiExtJSStoreTestBase
 						'type' => 'date',
 						'comparison' => 'gt',
 						'field' => 'pv_ts',
-						// That's "Help:Test"
-						'value' => '20170102000000'
+						'value' => '20250101010100'
 					],
 					[
 						'type' => 'string',
